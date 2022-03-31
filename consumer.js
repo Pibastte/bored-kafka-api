043c1f0e364d26420b0d23e26b4e586de9f917b2
Baptiste Florentin <baptiste.0606@hotmail.fr>
2022-03-31 13:09:43 +0200

    app ready

---
+++ consumer.js
@@ -0,0 +1,30 @@
+const mongo = require('mongodb');
+
+const MongoClient = mongo.MongoClient;
+
+const url = 'mongodb://localhost:27017';
+
+async function main(){
+    const client = await MongoClient.connect(url, { useNewUrlParser: true }).catch(err => { console.log(err); });
+    const db = await client.db("bored");
+
+    try {
+        //print all entries
+        const data = await db.collection('activity').find({}).toArray();
+        console.log('All:', data, data.length);
+
+        //get last position + country
+
+        //iss speed avg
+
+    }
+    catch(err){
+        console.log(err);
+    }
+    finally {
+        await client.close();
+    }
+
+}
+
+main();
---
+++ producer.js
@@ -0,0 +1,39 @@
+const axios = require('axios').default;
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'test',
+  brokers: ['localhost:9092']
+})
+
+const producer = kafka.producer({
+    allowAutoTopicCreation: true,
+    transactionTimeout: 30000
+})
+
+async function getData() {
+    await producer.connect();
+
+    const { data } = await axios.get('https://www.boredapi.com/api/activity');
+
+    await producer.send({
+        topic: 'bored-base',
+        messages: [
+            { 
+                key: 'activity', 
+                value: `${JSON.stringify(
+                    {
+                        timestamp: data.activity
+                    }
+                        
+                )}`
+                , partition: 0
+            },
+        ],
+    
+    });
+
+    console.log(`New thing to do ${data.activity}`)
+}
+
+setInterval(getData, 5100); //5.1s
